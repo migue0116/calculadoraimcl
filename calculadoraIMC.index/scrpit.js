afcaa056f58a2bea4c5886589ff1f400d58b285a
migue0116 <miguel.dolata@escola.pr.gov.br>
2026-05-08 11:32:17 -0300

    Update scrpit.js

--- calculadoraIMC.index/scrpit.js
+++ calculadoraIMC.index/scrpit.js
@@ -5,7 +5,21 @@ let botao = document.getElementById('botao');
 
 let classificacao = "";
 
-botao.onclick =() => {let imc = number(peso.value) / number (altura.value*altura.value)
+
+botao.onclick =() => {
+    let pesonovo = peso.value.trim();
+    let pesoformatado = pesonovo.replace(",",".")
+    if(pesonovo == "" || isNaN (pesoformatado)){
+        alert("corrija os campos")
+        return
+    }
+    let alturanovo = altura.value.trim()
+    let alturaformatado = alturanovo.replace(",",".")
+    if(alturanovo == "" || isNaN(alturaformatado)){
+        alert("corrija os campos")
+        return
+    }
+    let imc = Number(pesoformatado) / Number (alturaformatado*alturaformatado)
 if (imc<18.5){
     classificacao = "baixo peso"
 }else if ((imc>18.5) && (imc<=24.9)){
@@ -19,6 +33,8 @@ if (imc<18.5){
 }else {
     classificacao = "obesidade 3"
 }
-resultado.innerText = 'seu imc e: ${imc} \n classsificacao: ${classificacao}'
+resultado.innerText = `seu imc e: ${imc.toFixed(2)} \n classsificacao: ${classificacao}`
+peso.value = "";
+altura.value = "";
 
 }
